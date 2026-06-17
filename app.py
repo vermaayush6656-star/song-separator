a4b00b2a3a56905123cd8f5438f08990245fb78e
vermaayush6656-star <vermaayush6656@gmail.com>
2026-06-17 19:32:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,7 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
